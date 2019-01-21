bfffe32e37c509f2db02749636a1c29235f508c2
Matthew Paletta <mattpaletta@gmail.com>
2019-01-20 21:31:25 -0800

    Updated license and setup

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="pqdict",
     version="0.0.1",
@@ -11,13 +14,17 @@ setup(
     author="Matthew Paletta",
     author_email="mattpaletta@gmail.com",
     description="Thread-Safe PQDict Implementation",
-    license="BSD",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    license="GNU GPLv3",
+    python_requires='>=3.6',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        "Development Status :: 3 - Alpha",
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Communications',
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
+        "Natural Language :: English",
     ]
 )
